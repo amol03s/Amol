a2af17c3b1dfb6b9432ec316f53aa5cd2878e014
amol03s <amolthawal4444@gmail.com>
2019-11-13 07:05:34 +0530

    EditedFromLapi

--- JavaWorkspace/JDBCLayered/src/com/training/dao/MyConnection.java
+++ JavaWorkspace/JDBCLayered/src/com/training/dao/MyConnection.java
@@ -1,24 +1,24 @@
 package com.training.dao;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MyConnection {
 	public static Connection conn=null; 
-	public static Connection getMyConnection() {
-		if(conn==null) {
+	public static Connection getMyConnection(){
+		if(conn==null){
 			try {
 				DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-			    String url="jdbc:mysql://192.168.10.71:3307/dac13";
-			    conn=DriverManager.getConnection(url,"dac13","welcome");
-			} catch (SQLException e) {
+			    String url="jdbc:mysql://127.0.01:3306/user01";
+			    conn=DriverManager.getConnection(url,"root","root123");
+			    System.out.println("Connection Done Succesfully");
+			}catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
 		return conn;
-	}
-	public static void closeMyConnection() {
+}
+	public static void closeMyConnection(){
 		if(conn!=null) {
 			try {
 				conn.close();
@@ -27,5 +27,4 @@ public class MyConnection {
 			}
 		}
 	}
-
 }
