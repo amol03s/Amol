487b9fd9e51c7ebf7b978ebc269b39d2c8d245ca
amol03s <amolthawal4444@gmail.com>
2019-11-21 22:42:08 +0530

    Changes

--- AdvanceJava/ProductCategoryCrud/src/com/training/dao/LoginDao.java
+++
@@ -1,8 +0,0 @@
-package com.training.dao;
-
-import com.training.beans.MyUser;
-
-public interface LoginDao {
-	
-	public MyUser validateUser(String u, String p);
-}
--- AdvanceJava/ProductCategoryCrud/src/com/training/service/LoginService.java
+++
@@ -1,6 +0,0 @@
-package com.training.service;
-
-public interface LoginService {
-
-		public boolean validate(String u, String p);
-}
